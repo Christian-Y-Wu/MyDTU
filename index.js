506a933787f6395472bda098b2f2fbc04219dab3
s194597 <s194597@student.dtu.dk>
2022-11-08 16:13:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/redirecturi', async (req, res) => {
     const code = req.query.code;
     console.log("Code: " + code);
     console.log("Redirecting to application");
-    res.rediret('https://mydtu-app-server.herokuapp.com');
+    res.redirect('https://mydtu-app-server.herokuapp.com');
     // res.redirect('https://10.0.2.2:3434/redirecturi?code='+code);
     // res.redirect('https://mydtu-app-server.herokuapp.com/redirecturi?code='+code);
     
