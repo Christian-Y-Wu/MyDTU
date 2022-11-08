f5101ef32f842c2c5c70122a4e320b636b5bf9c2
s194597 <s194597@student.dtu.dk>
2022-11-08 14:26:13 +0100

    Added server links

---
+++ index.js
@@ -0,0 +1,42 @@
+const cors = require("cors");
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
+const app = express();
+const port = 3434;
+const assetlinks = fs.readFileSync('.well-known/assetlinks.json');
+const options = {
+    key:fs.readFileSync("cert/localhost-key.pem"),
+    cert:fs.readFileSync("cert/localhost.pem"),
+};
+
+app.use(cors());
+app.use(express.json());
+
+https
+    .createServer(options, app)
+    .listen(port, () => {
+        console.log(`Brightspace-OAuth-Server-NodeJS listening on port ${port}`)
+    })
+
+// app.listen(port,() => {
+//     console.log()
+// })
+
+app.get('/.well-known/assetlinks.json', function(req, res, next){
+    res.set('Content-Type', 'application/json');
+    res.status(200).send(assetlinks);
+})
+
+app.get('/redirecturi', async (req, res) => {
+    const code = req.query.code;
+    console.log("Code: " + code);
+    console.log("Redirecting to application");
+    // // res.redirect('https://google.dk');
+    // res.rediret('https://10.0.2.2:3434)    
+    // res.redirect('https://10.0.2.2:3434/redirecturi?code='+code);
+
+})
+
